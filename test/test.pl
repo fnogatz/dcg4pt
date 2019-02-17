a1d41765a50012a1c420ac07bb1f81132a3fcd0a
Falco Nogatz <fnogatz@gmail.com>
2019-02-17 03:33:47 +0100

    Re-add old tests

--- test/test.pl
+++ test/test.pl
@@ -1,9 +1,16 @@
 :- op(800, xfx, <=>).
 
+heads(DCGBody, In, Head1, Head2) :-
+  In = [First|_], integer(First), !,
+  format(atom(Head1), '~w > "~s"', [DCGBody, In]),
+  format(atom(Head2), '~w < "~s"', [DCGBody, In]).
+heads(DCGBody, In, Head1, Head2) :-
+  format(atom(Head1), '~w > ~w', [DCGBody, In]),
+  format(atom(Head2), '~w < ~w', [DCGBody, In]).
+
 % Define term expansion for TAP tests
 term_expansion(DCGBody: PT <=> In, [(Head1 :- Test1), (Head2 :- Test2)]) :-
-  format(atom(Head1), '~w > "~s"', [DCGBody, In]),
-  format(atom(Head2), '~w < "~s"', [DCGBody, In]),
+  heads(DCGBody, In, Head1, Head2),
   DCGBody =.. DCGBody_L,
   % build first test: from input to parsetree
   append(DCGBody_L, [PT1], DCGBody_L1),
@@ -27,6 +34,9 @@ term_expansion(DCGBody: PT <=> In, [(Head1 :- Test1), (Head2 :- Test2)]) :-
 :- use_module(library(dcg4pt/expand)).
 
 % DCGs to be tested starting here.
+
+%% Test Section I: DCGs
+
 c1 --> "a".
 c2 --> "abc".
 
@@ -42,26 +52,8 @@ d_or_e --> "e".
 
 or1 --> "d" | "e".
 
-% Example calls to be tested starting here.
+%% Test Section II: DCGs
 
-:- use_module(library(tap)).
-
-c1: c1("a") <=> `a`.
-c2: c2("abc") <=> `abc`.
-
-c(1): c("a") <=> `a`.
-c(2): c("abc") <=> `abc`.
-
-d_or_e: d_or_e("d") <=> `d`.
-d_or_e: d_or_e("e") <=> `e`.
-
-% or1: or1("d") <=> `d`.
-
-nonterminal: nonterminal(c1("a")) <=> `a`.
-nonterminal(1): nonterminal(c1("a")) <=> `a`.
-nonterminal(2): nonterminal(c2("abc")) <=> `abc`.
-
-/*
 sentence --> noun_phrase, verb_phrase.
 noun_phrase --> determiner, noun.
 verb_phrase --> ( verb ; verb, noun_phrase ).
@@ -83,72 +75,57 @@ sequences --> [other], sequence('*', single).
 
 sequence_star_prefix --> *single.
 
+% Example calls to be tested starting here.
+
 :- use_module(library(tap)).
 
-'single [some]' :-
-   phrase(single(X), [some]), !,
-   X = single(some).
+%% Test Section I: Tests
 
-'with_argument(1) [some]' :-
-   phrase(with_argument(1, X), [some]), !,
-   X = with_argument(some).
+c1: c1("a") <=> `a`.
+c2: c2("abc") <=> `abc`.
 
-'verb [eat]' :-
-   phrase(verb(X), [eat]), !,
-   X = verb(eat).
+c(1): c("a") <=> `a`.
+c(2): c("abc") <=> `abc`.
 
-'verb_phrase as single verb' :-
-   phrase(verb_phrase(X), [eat]), !,
-   X = verb_phrase(verb(_)).
+d_or_e: d_or_e("d") <=> `d`.
+d_or_e: d_or_e("e") <=> `e`.
 
-'verb_phrase as conjunction' :-
-   phrase(verb_phrase(X), [eat, the, apple]), !,
-   X = verb_phrase([verb(_), noun_phrase(_)]).
+or1: or1("d") <=> `d`.
+or1: or1("e") <=> `e`.
 
-'sequence_star []' :-
-   phrase(sequence_star(X), []), !,
-   X = sequence_star([]).
+nonterminal: nonterminal(c1("a")) <=> `a`.
+nonterminal(1): nonterminal(c1("a")) <=> `a`.
+nonterminal(2): nonterminal(c2("abc")) <=> `abc`.
 
-'sequence_star [some]' :-
-   phrase(sequence_star(X), [some]), !,
-   X = sequence_star([single(some)]).
+% Test Section II: Tests
 
-'sequence_star [some,some]' :-
-   phrase(sequence_star(X), [some,some]), !,
-   X = sequence_star([single(some), single(some)]).
+single: single(some) <=> [some].
+with_argument(1): with_argument(some) <=> [some].
+verb: verb(eat) <=> [eat].
+verb_phrase: verb_phrase(verb(eat)) <=> [eat].
+sequence_star: sequence_star([]) <=> [].
+sequence_star: sequence_star([single(some)]) <=> [some].
+sequence_star: sequence_star([single(some), single(some)]) <=> [some,some].
+sequence_star_prefix: sequence_star_prefix([single(some), single(some)]) <=> [some,some].
+sequence_question: sequence_question([]) <=> [].
+sequence_question: sequence_question([single(some)]) <=> [some].
+sequences: sequences([other, single(some)]) <=> [other,some].
+sentence:
+  sentence([
+    noun_phrase([
+      determiner(the),
+      noun(boy)
+    ]),
+    verb_phrase([
+      verb(eats),
+      noun_phrase([
+        determiner(the),
+        noun(apple)
+      ])
+    ])
+  ]) <=> [the, boy, eats, the, apple].
 
-'sequence_star_prefix [some,some]' :-
-   phrase(sequence_star_prefix(X), [some,some]), !,
-   X = sequence_star_prefix([single(some), single(some)]).
-
-'sequence_question []' :-
-   phrase(sequence_question(X), []), !,
-   X = sequence_question([]).
-
-'sequence_question [some]' :-
-   phrase(sequence_question(X), [some]), !,
-   X = sequence_question([single(some)]).
-
+/*
 'sequence_question (not multiples) [some,some]' :-
    \+phrase(sequence_question(_), [some, some]), !.
-
-'sequences [other, some]' :-
-   phrase(sequences(X), [other, some]), !,
-   X = sequences([other, single(some)]).
-
-'sentence [the, boy, eats, the, apple]' :-
-   phrase(sentence(X), [the, boy, eats, the, apple]), !,
-   X = sentence([
-      noun_phrase([
-         determiner(the),
-         noun(boy)
-      ]),
-      verb_phrase([
-         verb(eats),
-         noun_phrase([
-            determiner(the),
-            noun(apple)
-         ])
-      ])
-   ]).
 */
