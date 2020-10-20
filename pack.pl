67e24c01a34b3d69cc519f874066188ed8550987
Falco Nogatz <fnogatz@gmail.com>
2020-10-21 00:32:07 +0200

    v1.0.0

--- pack.pl
+++ pack.pl
@@ -1,11 +1,10 @@
 name(dcg4pt).
 
-version('0.2.0').
+version('1.0.0').
 download('https://github.com/fnogatz/dcg4pt/releases/*.zip').
 
 title('DCG for Parse Trees').
 author('Falco Nogatz','fnogatz@gmail.com').
-author('Dietmar Seipel','seipel@informatik.uni-wuerzburg.de').
 maintainer('Falco Nogatz','fnogatz@gmail.com').
 packager('Falco Nogatz','fnogatz@gmail.com').
 
